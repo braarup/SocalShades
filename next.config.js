fc3f99586bd2f0e424c7f94254decec470ffde0b
braarup <braarup@gmail.com>
2025-11-04 21:32:57 -0800

    Add HTTPS redirect configuration

--- next.config.js
+++ next.config.js
@@ -3,6 +3,22 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'header',
+            key: 'x-forwarded-proto',
+            value: 'http',
+          },
+        ],
+        destination: 'https://socalshadeswholesale.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
